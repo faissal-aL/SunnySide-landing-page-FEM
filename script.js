cedec32ec286d2ef4a92b2819159ecfed94bddf7
faissal-aL <43959576+faissal-aL@users.noreply.github.com>
2023-01-03 17:06:03 +0100

    fixed disappearing menu bug

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ menuButton.addEventListener("click",function(){
     if(menu.style.display == 'flex') menu.style.display = 'none';
     else menu.style.display = 'flex';
 });
+window.addEventListener("resize",function(){
+    if(this.screen.width > 500) menu.style.display = 'flex';
+    else menu.style.display = 'none';
+});
